b4500785ea62cb286a5a2d871b90e09b55f70b2d
MarcosAraujo <marcosbrasilbr@gmail.com>
2025-03-06 23:10:29 -0300

    atualização foto

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,17 @@
 import { useState } from 'react'
-
-import './App.css'
+import './App.css'  // Certifique-se de que a fonte é importada corretamente no CSS
 import Header from './components/Header/Header'
-
 import Fooder from './components/Fooder/Fooder'
 import Conteiner from './components/Conteiner/Conteiner'
-
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-    <Header />
-    <Conteiner />
-     
-     
-     <Fooder />
+      <Header />
+      <Conteiner />
+      <Fooder />
     </>
   )
 }
